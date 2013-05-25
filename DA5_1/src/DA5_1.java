bee952e5fe6dcf3d475313f6a565ecdbfcedf803
Lemon-will <tabletennisboy_2315@yahoo.co.jp>
2013-05-26 05:53:04 +0900

    時間計測、およびコメント追加

--- DA5_1/src/DA5_1.java
+++ DA5_1/src/DA5_1.java
@@ -74,7 +74,7 @@ class UnionFind {
 public class DA5_1 {
 
 	/**
-	 * @param args
+	 * 最小全域木問題、虱潰し解法
 	 */
 	public static boolean end = false;
 
@@ -84,11 +84,12 @@ public class DA5_1 {
 		int n = 0, m = 0;// n:端子数、m:辺数
 		int tmp = 0; // 重さ測定用変数
 		int min = 0; // 最終的な最小値
+		// 入力
 		n = scan.nextInt();
 		m = scan.nextInt();
-		boolean count[] = new boolean[m];
-		boolean flag_all = true;
-		boolean flag_tree = true;
+		boolean count[] = new boolean[m];// 2進数カウンタ
+		boolean flag_all = true;// 調査中の木が全域木かどうか
+		boolean flag_tree = true;// 調査中の木が木になっているかどうか
 		Arrays.fill(count, false);
 		ArrayList<Edge> A = new ArrayList<Edge>(); // 入力される辺のリスト
 		ArrayList<Edge> C = new ArrayList<Edge>(); // 調査を実行する辺のリスト
@@ -101,37 +102,38 @@ public class DA5_1 {
 			w[i] = scan.nextInt();
 		}
 		scan.close();
-		for (int i = 0; i < m; i++) {
+		for (int i = 0; i < m; i++) {// EdgeオブジェクトをAという動的配列に代入。配列の都合上頂点は本来のNo.から1引いている
 			A.add(new Edge(vi[i] - 1, vj[i] - 1, w[i]));
 			min += A.get(i).w;
 		}
-		min++;
-
+		min++;// 最小値の値は最初は最大値+1を代入しておく
+		long start = System.currentTimeMillis();
 		// 判定
 		while (end == false) {
-			UnionFind uf = new UnionFind(n);
-			uf.Init(n);
+			UnionFind uf = new UnionFind(n); // ループ毎にインスタンス生成
+			// 初期化
 			flag_all = true;
 			flag_tree = true;
 			C.clear();
 			tmp = 0;
-			for (int i = 0; i < m; i++) {
+			for (int i = 0; i < m; i++) {// Cという動的配列にカウンタがtrueのAを加える
 				if (count[i] == true)
 					C.add(A.get(i));
 			}
+
 			// ここからループになっていないかを判断
 			for (int i = 0; i < C.size(); i++) {
-				if (uf.same(C.get(i).vi, C.get(i).vj)) {
+				if (uf.same(C.get(i).vi, C.get(i).vj)) {// UFが同じグループに属している場合ループができているので木ではない
 					flag_tree = false;
 					break;
-				} else {
+				} else {// ループがなければ頂点同士をuniteし、重みを加算する
 					tmp += C.get(i).w;
 					uf.unite(C.get(i).vi, C.get(i).vj);
 				}
 			}
 			// ループがない場合は全域木かどうかを調査
 			if (flag_tree == true) {
-				for (int i = 1; i < n; i++) {
+				for (int i = 1; i < n; i++) {// すべての頂点が同じグループに属していない場合は辺でつながっていない頂点があるので全域木ではない
 					if (!uf.same(i, i - 1)) {
 						flag_all = false;
 						break;
@@ -141,14 +143,16 @@ public class DA5_1 {
 			// ループがなく、全域木である場合のみに最小値の更新
 			if ((flag_tree == true) && (flag_all == true)) {
 				min = Math.min(min, tmp);
-				System.out.println(min);
 			}
-			func(count, 0, m);
+			func(count, 0, m);// カウンタ加算
 		}
+		long stop = System.currentTimeMillis();
+		// 出力
 		System.out.println(min);
+		System.out.println("Time: " + (stop - start) + "ms");
 	}
 
-	private static boolean[] func(boolean A[], int i, int n) {
+	private static boolean[] func(boolean A[], int i, int n) {// 2進数インクリメントメソッド
 		if (i >= n) {
 			end = true;
 			return A;
