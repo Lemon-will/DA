d31f811388c381145cab90aa56dae62c45691162
Lemon-will <tabletennisboy_2315@yahoo.co.jp>
2013-07-10 06:19:54 +0900

    深さ優先までできた

--- DA12_1/src/DA12_1.java
+++ DA12_1/src/DA12_1.java
@@ -1,38 +1,93 @@
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.PriorityQueue;
+import java.util.Scanner;
 
 public class DA12_1 {
+	public static int time = 0;
+	public static int n = 0;
+	public static int m = 0;
+	public static Vertices[] V;
+	public static Edge[] E;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		PriorityQueue<Integer> que = new PriorityQueue<Integer>(1, new Mycomp());
-		int[] a = { 4, 2, 5, 7, 2, 1, 3, 10 };
-
-		for (int i = 0; i < a.length; i++) {
-			que.add(a[i]);
+		PriorityQueue<Integer> Q = new PriorityQueue<Integer>(1, new Mycomp());
+		Scanner scan = new Scanner(System.in);
+		n = scan.nextInt();
+		m = scan.nextInt();
+		V = new Vertices[m];
+		E = new Edge[n];
+		for (int i = 0; i < m; i++) {
+			V[i] = new Vertices(i);
+		}
+		for (int i = 0; i < n; i++) {
+			E[i] = new Edge();
 		}
 
-		int[] b = new int[a.length];
-		for (int i = 0; i < b.length; i++) {
-			b[i] = (int) que.poll();
+	}
+
+	public static void DFS() {
+		for (int i = 0; i < V.length; i++) {
+			V[i].color = Vertices.WHITE;
+			V[i].pi = null;
 		}
-		for (int i = 0; i < b.length; i++) {
-			System.out.println(b[i]);
+		time = 0;
+		for (int i = 0; i < V.length; i++) {
+			if (V[i].color == Vertices.WHITE)
+				DFS_Visit(V[i]);
 		}
 	}
 
+	public static void DFS_Visit(Vertices u) {
+		ArrayList<Vertices> A = new ArrayList<Vertices>();
+		time++;
+		u.d = time;
+		u.color = Vertices.GRAY;
+		A = Adj(u);
+		for (int i = 0; i < A.size(); i++) {
+			if (A.get(i).color == Vertices.WHITE) {
+				A.get(i).pi = u;
+				DFS_Visit(A.get(i));
+			}
+		}
+		u.color = Vertices.BLACK;
+		time++;
+		u.f = time;
+	}
+
+	private static ArrayList<Vertices> Adj(Vertices u) {
+		ArrayList<Vertices> list = new ArrayList<Vertices>();
+		for (int i = 0; i < n; i++) {
+			if (E[i].u.equals(u)) {
+				list.add(E[i].v);
+			}
+		}
+		return list;
+	}
+
+	private static void Init(Vertices s) {
+		for (int i = 0; i < n; i++) {
+			V[i].d = 3 * n;
+			V[i].pi = null;
+		}
+		s.d = 0;
+	}
+	
+	private void Relax(Vertices u,Vertices v,int w){
+	}
 }
 
-class Mycomp implements Comparator {
+class Mycomp implements Comparator<Integer> {
 
 	@Override
-	public int compare(Object o1, Object o2) {
+	public int compare(Integer o1, Integer o2) {
 		// TODO 自動生成されたメソッド・スタブ
-		int i = (Integer) o1;
-		int j = (Integer) o2;
+		int i = o1;
+		int j = o2;
 
 		if (i > j) {
 			return 1;
@@ -45,13 +100,26 @@ class Mycomp implements Comparator {
 
 }
 
-class Edge {
-	int u, v, w;
+class Vertices {
+	public final static int WHITE = 0;
+	public final static int GRAY = 1;
+	public final static int BLACK = 2;
+	int index;
+	int d, f;
+	int color;
+	Vertices pi;
 
-	Edge(int u_, int v_, int w_) {
-		u_ = u;
-		v_ = v;
-		w_ = w;
+	public Vertices(int id) {
+		// TODO 自動生成されたコンストラクター・スタブ
+		index = id;
+		color = WHITE;
+		pi = null;
 	}
+
+}
+
+class Edge {
+	Vertices u, v;
+	int w;
 
 }
