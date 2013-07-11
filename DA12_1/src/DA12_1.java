1e11a316984efc79085b31d1281fd18462241f0d
Lemon <tabletennisboy_2315@yahoo.co.jp>
2013-07-11 12:17:13 +0900

    一時完成版。 DFSいらなかったね(´・ω・｀)

--- DA12_1/src/DA12_1.java
+++ DA12_1/src/DA12_1.java
@@ -16,10 +16,11 @@ public class DA12_1 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		Scanner scan = new Scanner(System.in);
-		n = scan.nextInt();
 		m = scan.nextInt();
+		n = scan.nextInt();
 		V = new ArrayList<Vertices>();
 		E = new Edge[n];
+		int[][] e_data = new int[3][n];
 		for (int i = 0; i < m; i++) {
 			V.add(new Vertices(i));
 		}
@@ -27,8 +28,24 @@ public class DA12_1 {
 			E[i] = new Edge();
 		}
 
-	}
+		for (int i = 0; i < n; i++) {
+			e_data[0][i] = scan.nextInt();
+			e_data[1][i] = scan.nextInt();
+			e_data[2][i] = scan.nextInt();
+			E[i].u = V.get(e_data[0][i]);
+			E[i].v = V.get(e_data[1][i]);
+			E[i].w = e_data[2][i];
+		}
+		scan.close();
 
+		Dijkstra(V.get(0));
+
+		for (int i = 1; i < V.size(); i++) {
+			System.out.println(i + " " + V.get(i).d);
+		}
+
+	}
+/*
 	public static void DFS() {
 		for (int i = 0; i < V.size(); i++) {
 			V.get(i).color = Vertices.WHITE;
@@ -46,7 +63,7 @@ public class DA12_1 {
 		time++;
 		u.d = time;
 		u.color = Vertices.GRAY;
-		A = Adj(u);
+//		A = Adj(u);
 		for (int i = 0; i < A.size(); i++) {
 			if (A.get(i).color == Vertices.WHITE) {
 				A.get(i).pi = u;
@@ -57,47 +74,52 @@ public class DA12_1 {
 		time++;
 		u.f = time;
 	}
-
-	private static ArrayList<Vertices> Adj(Vertices u) {
-		ArrayList<Vertices> list = new ArrayList<Vertices>();
+*/
+	private static ArrayList<Edge> Adj(Vertices u) {
+		ArrayList<Edge> list = new ArrayList<Edge>();
 		for (int i = 0; i < n; i++) {
 			if (E[i].u.equals(u)) {
-				list.add(E[i].v);
+				list.add(E[i]);
 			}
 		}
 		return list;
 	}
 
 	private static void Init(Vertices s) {
-		for (int i = 0; i < n; i++) {
-			V.get(i).d = 3 * n;
+		int tmp = 0;
+		for (int i = 0; i < E.length; i++) {
+			tmp += E[i].w;
+		}
+		for (int i = 0; i < V.size(); i++) {
+			V.get(i).d = tmp + 1;
 			V.get(i).pi = null;
 		}
 		s.d = 0;
 	}
 
-	private static void Relax(Vertices u, Vertices v, int w) {
-		if (v.d > u.d + w) {
-			v.d = u.d + w;
+	private static void Relax(Vertices u, Vertices v, Edge E) {
+		if (v.d > u.d + E.w) {
+			v.d = u.d + E.w;
 			v.pi = u;
 		}
 	}
 
-	private static void Dijkstra(int w, Vertices s) {
+	private static void Dijkstra(Vertices s) {
 		PriorityQueue<Vertices> Q = new PriorityQueue<Vertices>(1, new Mycomp());
 		ArrayList<Vertices> S = new ArrayList<Vertices>();
+		ArrayList<Edge> A = new ArrayList<Edge>();
 		Init(s);
 		S.clear();
 		for (int i = 0; i < V.size(); i++) {
 			Q.add(V.get(i));
 		}
-		while (Q != null) {
+		while (!Q.isEmpty()) {
 			Vertices u = Q.poll();
 			S.add(u);
-			for (int i = 0; i < E.length; i++) {
-				if(u.equals(E[i].u)){
-					Relax(u,E[i].v,w);//wについて再考する必要あり
-				}
+			A.clear();
+			A = Adj(u);
+			for (int i = 0; i < A.size(); i++) {
+				Relax(u,A.get(i).v,A.get(i));
 			}
 		}
 
